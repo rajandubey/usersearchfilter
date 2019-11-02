871408e927f0b99cef4763f183353545d70ff815
rajandubey <rajandubey200@gmail.com>
2019-11-02 13:53:22 +0530

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { UsersComponent } from './users/users.component';
+import { ToDosComponent } from './to-dos/to-dos.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, UsersComponent, ToDosComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
