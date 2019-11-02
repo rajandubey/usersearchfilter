802aa0dee85e01eca5982139a2fd4f3a6022ee2d
rajandubey <rajandubey200@gmail.com>
2019-11-02 14:17:34 +0530

    2 files modified, 2 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { UsersComponent } from './users/users.component';
 import { ToDosComponent } from './to-dos/to-dos.component';
 import { NgwWowModule } from 'ngx-wow';
+import { AboutComponent } from './about/about.component';
 
+const routers:Routes=[
+  {
+    path:'',
+    component:UsersComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'users',
+    component:UsersComponent
+  },
+  {
+    path:'todo',
+    component:ToDosComponent
+  }
+]
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,NgwWowModule,RouterModule ],
-  declarations: [ AppComponent, HelloComponent, UsersComponent, ToDosComponent ],
+  imports:      [ BrowserModule, FormsModule,NgwWowModule,RouterModule.forRoot(routers) ],
+  declarations: [ AppComponent, UsersComponent, ToDosComponent, AboutComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
